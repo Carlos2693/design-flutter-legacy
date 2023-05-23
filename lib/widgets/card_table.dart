558a7a9b5c576b9652644fdb95b72aa8558bea87
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2023-05-23 06:26:59 -0600

    [FCB] Diseño compuesto - Blur

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CardTable extends StatelessWidget {
@@ -30,22 +32,33 @@ class _Siglecard extends StatelessWidget {
     
     return Container(
       margin: EdgeInsets.all(15),
-      height: 180,
-      decoration: BoxDecoration(
-        color: Color.fromRGBO(62, 66, 107, 0.7),
-        borderRadius: BorderRadius.circular(20)
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            backgroundColor: color,
-            child: Icon(icon, size: 35,),
-            radius: 30,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: 5,
+            sigmaY: 5
           ),
-          SizedBox(height: 10,),
-          Text(text, style: TextStyle(color: color, fontSize: 18),)
-        ],
+          child: Container(
+            height: 180,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(62, 66, 107, 0.7),
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundColor: color,
+                  child: Icon(icon, size: 35, color: Colors.white,),
+                  radius: 30,
+                ),
+                SizedBox(height: 10,),
+                Text(text, style: TextStyle(color: color, fontSize: 18),)
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
