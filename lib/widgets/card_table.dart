7688c46133f04e4079461f2b1fc691fb29078678
Carlos Buruel Ortiz <carlos.ob.26@gmail.com>
2023-05-23 06:37:06 -0600

    [FCB] Optimizaciones

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -3,60 +3,77 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class CardTable extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Table(
       children: [
-        TableRow(
-          children: [
-            _Siglecard(color: Colors.blue, icon: Icons.border_all, text: 'General'),
-            _Siglecard(color: Colors.pinkAccent, icon: Icons.car_rental, text: 'Transport'),
-          ]
-        )
+        TableRow(children: [
+          _Siglecard(
+              color: Colors.blue, icon: Icons.border_all, text: 'General'),
+          _Siglecard(
+              color: Colors.pinkAccent,
+              icon: Icons.car_rental,
+              text: 'Transport'),
+        ])
       ],
     );
   }
 }
 
 class _Siglecard extends StatelessWidget {
-
   final IconData icon;
   final Color color;
   final String text;
 
-  const _Siglecard({super.key, required this.icon, required this.color, required this.text});
+  const _Siglecard(
+      {super.key, required this.icon, required this.color, required this.text});
 
   @override
   Widget build(BuildContext context) {
-    
+    var column = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          backgroundColor: color,
+          child: Icon(
+            icon,
+            size: 35,
+            color: Colors.white,
+          ),
+          radius: 30,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          text,
+          style: TextStyle(color: color, fontSize: 18),
+        )
+      ],
+    );
+    return _CardBackground(child: column);
+  }
+}
+
+class _CardBackground extends StatelessWidget {
+  final Widget child;
+
+  const _CardBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
         child: BackdropFilter(
-          filter: ImageFilter.blur(
-            sigmaX: 5,
-            sigmaY: 5
-          ),
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
           child: Container(
             height: 180,
             decoration: BoxDecoration(
-              color: Color.fromRGBO(62, 66, 107, 0.7),
-              borderRadius: BorderRadius.circular(20)
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundColor: color,
-                  child: Icon(icon, size: 35, color: Colors.white,),
-                  radius: 30,
-                ),
-                SizedBox(height: 10,),
-                Text(text, style: TextStyle(color: color, fontSize: 18),)
-              ],
-            ),
+                color: Color.fromRGBO(62, 66, 107, 0.7),
+                borderRadius: BorderRadius.circular(20)),
+            child: child,
           ),
         ),
       ),
